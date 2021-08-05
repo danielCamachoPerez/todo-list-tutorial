1924cd26a13ffbcf8069e2a9a7f774a057b6cf4d
Daniel Camacho <danielcamachodev@outlook.com>
2021-08-05 15:49:06 -0500

    create

--- js/main.js
+++ js/main.js
@@ -1,14 +1,39 @@
 window.onload = () => {
     const todos = []
     const form = document.getElementById('form')
-    const todo = document.getElementById('todo')
+    const todoInput = document.getElementById('text')
+    const done = document.getElementById('done')
+    const todosText = document.getElementById('todos')
 
     const todoSubmit = (e) => {
         e.preventDefault()
-        const txt = todo.value
-        console.log(txt)
-        todo.value = ''
+        const title = todoInput.value
+        const status = false
+        todos.push({ title, status })
+        render()
+        todoInput.value = ''
+    }
+    const render = () => {
+        const template = todos.map(todo => `
+        <div class="todos">
+            <p class="subtitle">
+                <strong>${todo.title}</strong>
+            </p>
+            <label class="checkbox">
+                <input type="checkbox" id="done">
+                Done
+            </label><br/>
+            <button type="button" class="button">Edit</button>
+            <button type="button" class="button is-danger">Delete</button>
+        </div>
+        `)
+
+        todosText.innerHTML = template.join('')
     }
 
+    const todoSearch = (e) => {
+        e.preventDefault()
+    }
     form.addEventListener('submit', e => todoSubmit(e))
+    form.addEventListener('submit', e => todoSearch(e))
 }
